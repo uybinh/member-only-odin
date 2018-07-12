d80aca176e5e9dc22f9c7ec5ee5adb4501d38fb4
Uy Binh <uybinh@gmail.com>
2018-07-12 14:29:21 +0700

    Edit seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -88,6 +88,18 @@ namespace :deploy do
       end
     end
   end
+
+
+  desc 'Seed the database'
+  task :seed do
+    on primary :db do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
   
 
   desc 'Restart application'
