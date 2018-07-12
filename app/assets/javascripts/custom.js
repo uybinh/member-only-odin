1032fefb43ff2acf1e53287b3dd7f9ebc1c7f59f
Uy Binh <uybinh@gmail.com>
2018-07-13 05:32:29 +0700

    Edit script load on site load

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
 
   // Get all "navbar-burger" elements
   var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
@@ -22,4 +22,4 @@ window.onload = function () {
     });
   }
 
-};
+});
