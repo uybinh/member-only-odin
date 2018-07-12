4b83ee7d676d66d7d3984746752910e6a2c61478
Uy Binh <uybinh@gmail.com>
2018-07-12 10:13:23 +0700

    Add seed task to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -78,6 +78,11 @@ namespace :deploy do
     end
   end
 
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
